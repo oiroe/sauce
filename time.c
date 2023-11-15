a95e566a63924391ab406a7b47464b84a1287b1b
oiroe <pathiti.b@gmail.com>
2023-11-16 00:13:15 +0700

    meaw

--- time.c
+++ time.c
@@ -17,7 +17,7 @@ long	get_time(void)
 	struct timeval	time;
 
 	gettimeofday(&time, NULL);
-	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+	return ((time.tv_sec * 1000000) + (time.tv_usec));
 }
 
 long	now(long past)
