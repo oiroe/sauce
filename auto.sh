9ba391771fb2a83f24eddf6c9d24dd016514b6e7
oiroe <pathiti.b@gmail.com>
2023-11-15 23:35:49 +0700

    sort and colour added

--- auto.sh
+++ auto.sh
@@ -8,7 +8,11 @@ fi
 # Set the path to the folder containing the files to be tested
 test_folder="$1"
 mode="$2"
-S
+
+if [[ $mode -lt 1 || $mode -gt 5 ]]; then
+	exit 1
+fi
+
 # Build using Makefile
 make
 
@@ -20,10 +24,28 @@ else
     exit 1
 fi
 
+Off="\033[0m"
+if [ $mode -eq 1 ]; then
+	CYN="\e[0;36m"
+	echo -e "${CYN}Bubble Sort${Off}"
+elif [ $mode -eq 2 ]; then
+	PUR="\033[0;35m"
+	echo -e "${PUR}Insertion Sort${Off}"
+elif [ $mode -eq 3 ]; then
+	BLU="\e[0;34m"
+	echo -e "${BLU}Selection Sort${Off}"
+elif [ $mode -eq 4 ]; then
+	YEL="\e[0;33m"
+	echo -e "${YEL}Merge Sort${Off}"
+elif [ $mode -eq 5 ]; then
+	RED="\e[0;31m"
+	echo -e "${RED}Quick Sort${Off}"
+fi
+
 # Loop through each file in the test folder
 for file in "$test_folder"/*; do
     if [ -f "$file" ]; then
-        echo "Testing $file"
+        echo -e "Testing $file"
         # Add your test command here, for example:
         ./sort "$file" "$mode"
     fi
